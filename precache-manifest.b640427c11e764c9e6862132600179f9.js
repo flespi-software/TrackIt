fc251f080e665a561235f20d8aab91e758670191
Sergei Buntsevich <sebu@gurtam.com>
2018-07-27 17:18:41 +0300

    publish: [update] packages [feature] migrate to new version of quasar [fix] init few devices on start
    generated from commit 49b25431f237acb30447168e33b4003bcace9679

---
+++ precache-manifest.b640427c11e764c9e6862132600179f9.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "64baba060d5a08a517f783c929f04167",
+    "url": "img/corpse.64baba0.png"
+  },
+  {
+    "revision": "a4549b562771c6734445",
+    "url": "js/vendor.a4549b5.js"
+  },
+  {
+    "revision": "9a9bf03959ba3eba0bc007f6c7699a33",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "4ca6116897b45c5c22d8e3d3f21576f5",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "47fc2bc80d4dc7e7835423c0505302e7",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "906c193b6d193a65add774107b3923db",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "a9de7e81de000ac57c2855c29a98e796",
+    "url": "statics/police.png"
+  },
+  {
+    "revision": "e70b8108d642f3e1eda25442d0a2a629",
+    "url": "statics/manifest.json"
+  },
+  {
+    "revision": "c2f336f76202bb73019843c7ad316def",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "c81a3f3a982ad3175edf7a78d797ced3",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "d56df49a807a9fd06eb1667a84d3810e",
+    "url": "statics/GitHub-Mark-Light-32px.png"
+  },
+  {
+    "revision": "82f983ec2277961e9878f2bf014b13fc",
+    "url": "statics/flespi_logo_black.svg"
+  },
+  {
+    "revision": "4dbf2ed5128b084a0b6f9ce268c02558",
+    "url": "statics/trackit.png"
+  },
+  {
+    "revision": "ca2124cb4b54895f85dba5fbf32335f0",
+    "url": "statics/track-it-logo.png"
+  },
+  {
+    "revision": "f2e09a9743fdb0c7f4dcbe4242441a5d",
+    "url": "statics/right-graphite@2x.png"
+  },
+  {
+    "revision": "105ab0c48f5ba92a1310652050a4c1e2",
+    "url": "statics/police50.png"
+  },
+  {
+    "revision": "1e3d1da4cf6d85e6e28cc3aae77e3ead",
+    "url": "statics/mountain.svg"
+  },
+  {
+    "revision": "f87561b8bb354ef83b09a66e54f70e08",
+    "url": "statics/GitHub-Mark-32px.png"
+  },
+  {
+    "revision": "64baba060d5a08a517f783c929f04167",
+    "url": "statics/corpse.png"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.e9dbbe8.woff"
+  },
+  {
+    "revision": "75567a2bf347748bc956582a3be2769e",
+    "url": "index.html"
+  },
+  {
+    "revision": "44564ed6157ae8624113",
+    "url": "js/1.44564ed.js"
+  },
+  {
+    "revision": "ef0717a8cb7330abc3d1",
+    "url": "js/2.ef0717a.js"
+  },
+  {
+    "revision": "35ddedfe04fddae701df",
+    "url": "js/app.e40c3ee.js"
+  },
+  {
+    "revision": "da2339ad444dee30e5ac",
+    "url": "js/4.da2339a.js"
+  },
+  {
+    "revision": "0384166eee4bdef859f0741812fac721",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.0384166.woff"
+  },
+  {
+    "revision": "91d265fb0b78f343dd3793775b1ff8a6",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.91d265f.woff2"
+  },
+  {
+    "revision": "098aafda4b974d4a6432c25c0e7b59a3",
+    "url": "fonts/mdi.098aafd.woff2"
+  },
+  {
+    "revision": "24a45ab82bdfa1fcc91c73c710129905",
+    "url": "fonts/mdi.24a45ab.woff"
+  },
+  {
+    "revision": "f2e09a9743fdb0c7f4dcbe4242441a5d",
+    "url": "img/right-graphite@2x.f2e09a9.png"
+  },
+  {
+    "revision": "4dbf2ed5128b084a0b6f9ce268c02558",
+    "url": "img/trackit.4dbf2ed.png"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.bafb105.woff"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.8c2ade5.woff"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.cf6613d.woff"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.a1471d1.woff"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.de8b743.woff"
+  },
+  {
+    "revision": "35ddedfe04fddae701df",
+    "url": "css/app.0e11d7a3.css"
+  },
+  {
+    "revision": "ef0717a8cb7330abc3d1",
+    "url": "css/2.fc008b89.css"
+  },
+  {
+    "revision": "44564ed6157ae8624113",
+    "url": "css/1.005ae18d.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,31 @@
-"use strict";var precacheConfig=[["app.f449b3d276d4e958f6c7591e1a9a4532.css","2910d991336960bbb01a59ca92f099ee"],["fonts/MaterialIcons-Regular.012cf6a.woff","012cf6a10129e2275d79d6adac7f3b02"],["fonts/Roboto-Bold.ad140ff.woff","ad140ff02a7091257e2b31619106194e"],["fonts/Roboto-Light.37fbbba.woff","37fbbbad5577a95bdf058307c717c882"],["fonts/Roboto-Medium.303ded6.woff","303ded6436dcf7ea75157e2aeff876ce"],["fonts/Roboto-Regular.081b11e.woff","081b11ebaca8ad30fd092e01451015dc"],["fonts/Roboto-Thin.90d3804.woff","90d3804f0231704c15ccc5861245e8ce"],["fonts/mdi.5d77f30.woff","5d77f30c080b096797c5cba6922a1510"],["img/corpse.64baba0.png","64baba060d5a08a517f783c929f04167"],["img/right-graphite@2x.f2e09a9.png","f2e09a9743fdb0c7f4dcbe4242441a5d"],["img/trackit.4dbf2ed.png","4dbf2ed5128b084a0b6f9ce268c02558"],["index.html","9101f3f8590b661a9d8fea5c0cd1da89"],["js/0.d1f2b6315d0cef0c6b54.js","71700046e1a807a66fb2ba3a3646a0d7"],["js/1.64f0cb09ef3885798c45.js","25d70a2ad27d4732415e1966e8c86e71"],["js/2.eb05d07bb2066e59f9d7.js","2fe9d6bbc34cdaa3202ac54a2727caab"],["js/app.216c84044c749ef4ed19.js","99b985071da657ee57212dcd3a0109ac"],["js/vendor.d2edcb8bdc31bba9cf31.js","84b7fbd6fe030ae3b927fec1b9ebc176"],["manifest.json","b3c6b5784dee9f1f4b0c403b7e1ee3fe"],["statics/GitHub-Mark-32px.png","f87561b8bb354ef83b09a66e54f70e08"],["statics/GitHub-Mark-Light-32px.png","d56df49a807a9fd06eb1667a84d3810e"],["statics/corpse.png","64baba060d5a08a517f783c929f04167"],["statics/flespi_logo_black.svg","82f983ec2277961e9878f2bf014b13fc"],["statics/icons/apple-icon-152x152.png","906c193b6d193a65add774107b3923db"],["statics/icons/favicon-16x16.png","c81a3f3a982ad3175edf7a78d797ced3"],["statics/icons/favicon-32x32.png","c2f336f76202bb73019843c7ad316def"],["statics/icons/icon-192x192.png","47fc2bc80d4dc7e7835423c0505302e7"],["statics/icons/icon-512x512.png","9a9bf03959ba3eba0bc007f6c7699a33"],["statics/icons/ms-icon-144x144.png","4ca6116897b45c5c22d8e3d3f21576f5"],["statics/manifest.json","e70b8108d642f3e1eda25442d0a2a629"],["statics/mountain.svg","1e3d1da4cf6d85e6e28cc3aae77e3ead"],["statics/police.png","a9de7e81de000ac57c2855c29a98e796"],["statics/police50.png","105ab0c48f5ba92a1310652050a4c1e2"],["statics/right-graphite@2x.png","f2e09a9743fdb0c7f4dcbe4242441a5d"],["statics/track-it-logo.png","ca2124cb4b54895f85dba5fbf32335f0"],["statics/trackit.png","4dbf2ed5128b084a0b6f9ce268c02558"]],cacheName="sw-precache-v3-track-it-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,a){var t=new URL(e);return"/"===t.pathname.slice(-1)&&(t.pathname+=a),t.toString()},cleanResponse=function(e){return e.redirected?("body"in e?Promise.resolve(e.body):e.blob()).then(function(a){return new Response(a,{headers:e.headers,status:e.status,statusText:e.statusText})}):Promise.resolve(e)},createCacheKey=function(e,a,t,n){var c=new URL(e);return n&&c.pathname.match(n)||(c.search+=(c.search?"&":"")+encodeURIComponent(a)+"="+encodeURIComponent(t)),c.toString()},isPathWhitelisted=function(e,a){if(0===e.length)return!0;var t=new URL(a).pathname;return e.some(function(e){return t.match(e)})},stripIgnoredUrlParameters=function(e,a){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(e){return a.every(function(a){return!a.test(e[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var a=e[0],t=e[1],n=new URL(a,self.location),c=createCacheKey(n,hashParamName,t,!1);return[n.toString(),c]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(e){return setOfCachedUrls(e).then(function(a){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!a.has(t)){var n=new Request(t,{credentials:"same-origin"});return fetch(n).then(function(a){if(!a.ok)throw new Error("Request for "+t+" returned a response with status "+a.status);return cleanResponse(a).then(function(a){return e.put(t,a)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var a=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(e){return e.keys().then(function(t){return Promise.all(t.map(function(t){if(!a.has(t.url))return e.delete(t)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(e){if("GET"===e.request.method){var a,t=stripIgnoredUrlParameters(e.request.url,ignoreUrlParametersMatching);(a=urlsToCacheKeys.has(t))||(t=addDirectoryIndex(t,"index.html"),a=urlsToCacheKeys.has(t));0,a&&e.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(t)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(a){return console.warn('Couldn\'t serve response for "%s" from cache: %O',e.request.url,a),fetch(e.request)}))}});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.b640427c11e764c9e6862132600179f9.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "track-it"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/^https:\/\/((localhost:9005)|(flespi\.io))\/(gw|auth|platform|storage|mqtt)\/.*/, workbox.strategies.networkOnly(), 'GET');
